3c50e5f22a5216aa4a45b4db220e89378dedb83c
bFraley <bFraley@users.noreply.github.com>
2016-10-31 09:48:47 -0400

    complete value comparisons tests and def Balance > 3 limit error

--- balance_scale.py
+++ balance_scale.py
@@ -1,10 +1,26 @@
+class BalanceUsedTooManyTimesError(Exception):
+    pass
+
 class BalanceScale():
+
+    def __init__(self):
+        self.times_left = 3
+
     def compare(self, left, right):
+        if len(left) != len(right):
+            raise ValueError
+
+        if self.times_left <= 0:
+            raise BalanceUsedTooManyTimesError
+
+        self.times_left -= 1
+
         left_sum = sum(left)
         right_sum = sum(right)
 
         if left_sum > right_sum:
             return -1
+        elif left_sum < right_sum:
+            return 1
         else:
             return 0
-            
--- test_balance_scale.py
+++ test_balance_scale.py
@@ -13,7 +13,21 @@ class BalanceScaleTestCase(unittest.TestCase):
         self.assertEqual(self.scale.compare(left, right, -1))
 
     def test_compare_returns_zero_when_left_equals_right(self):
-        left = [1, 1, 2]
+        left = [1, 1, 1]
         right = [1, 1, 1]
 
         self.assertEqual(self.scale.compare(left, right), 0)
+
+
+    def test_compare_returns_zero_when_left_less_than_right(self):
+        left = [1, 1, 0]
+        right = [1, 1, 1]
+
+        self.assertEqual(self.scale.compare(left, right), 1)
+
+
+    def test_compare_raises_ValueError_if_len_left_not_equal_len_right(self):
+        left = [1]
+        right = [1, 2, 3]
+
+        self.assertEqual(ValueError, self.scale.compare(left, right))
